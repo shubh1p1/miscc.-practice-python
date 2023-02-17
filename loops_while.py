bbdb9241de8dfaf493dcb22d1b0d105f5ab667de
Shubham <shubhampatil4994@gmail.com>
2023-02-17 13:41:36 +0530

    commit

---
+++ loops_while.py
@@ -0,0 +1,6 @@
+# print 5 times "Hello"
+i = 1
+while i<=5 :
+    print("Hello")
+  # i+=1
+    i=i+1
--- p1.py
+++ p1.py
@@ -1,3 +1,3 @@
-# odd even
+# odd even one line 
 
 print("Even" if int(input("Enter a number to check odd or even"))%2==0 else "odd")
